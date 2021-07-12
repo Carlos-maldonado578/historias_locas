258afdd87dcb32b03c367f2e7e90ea097e5f190c
Carlos-maldonado578 <carlos.maldonado7@gmail.com>
2021-07-11 21:05:54 -0400

    commit message

--- historias_locas.py
+++ historias_locas.py
@@ -3,8 +3,11 @@
 # Aprende a programar con: ________.
 
 
-organización = "freeCodecamp"
+# organización = "freeCodecamp"
 
-print("Aprende a programar con " + organización)
-print("Aprende a programar con {}".format(organización))
-print(f"Aprende a programar con {organización}") #f-string
+# print("Aprende a programar con " + organización)
+# print("Aprende a programar con {}".format(organización))
+# print(f"Aprende a programar con {organización}") #f-string
+
+
+madlib = f"¡Programar es tan {adj}! Siempre me emociona porque me encanta {verbo1} problemas. ¡Aprende a {verbo2} con freecodecamp y alcanza tus {sustantivo_plural}"
