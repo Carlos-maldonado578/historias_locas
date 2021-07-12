bbacb165fc6bacb29f1f1304cc47535465f75542
Carlos-maldonado578 <carlos.maldonado7@gmail.com>
2021-07-11 20:10:04 -0400

    Create historias_locas.py
    first commit

---
+++ historias_locas.py
@@ -0,0 +1,10 @@
+# Concatenar cadenas de caracteres.
+# Supongamos que queremos crear una cadena que diga:
+# Aprende a programar con: ________.
+
+
+organización = "freeCodecamp"
+
+print("Aprende a programar con " + organización)
+print("Aprende a programar con {}".format(organización))
+print(f"Aprende a programar con {organización}") #f-string
