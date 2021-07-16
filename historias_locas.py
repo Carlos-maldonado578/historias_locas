bb2614e55c0fc7f5542c32630220308b65c2b3b8
Carlos-maldonado578 <carlos.maldonado7@gmail.com>
2021-07-16 17:56:54 -0400

    agregando variables a la actividad

--- historias_locas.py
+++ historias_locas.py
@@ -10,4 +10,13 @@
 # print(f"Aprende a programar con {organización}") #f-string
 
 
+# Mad Libs (Historias Locas)
+adj = input("Adjetivo: ")
+verbo1 = input("Verbo: ")
+verbo2 = input("Verbo: ")
+sustantivo_plural = input("Sustantivo (Plural): ")
+
+
 madlib = f"¡Programar es tan {adj}! Siempre me emociona porque me encanta {verbo1} problemas. ¡Aprende a {verbo2} con freecodecamp y alcanza tus {sustantivo_plural}"
+
+print(madlib)
